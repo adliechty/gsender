66a5900dad97dc8390bc87518b80e15d31427601
unknown <tony.liechty@gmail.com>
2022-04-11 22:57:30 -0500

    docker image working, automated until end where need to execute commented commands as I don't have nvm working in docker yet

--- Docker/buildDocker.bat
+++ Docker/buildDocker.bat
@@ -1,1 +1,2 @@
-docker build --build-arg OPENCV_VERSION=4.x --build-arg NODEJS_MAJOR_VERSION=17 --build-arg WITH_CONTRIB=true -t gsenderdevdocker:1.0 .
+docker build --build-arg OPENCV_VERSION=4.1.0 --build-arg NODEJS_MAJOR_VERSION=17 -t gsenderdevdocker2:1.0 .
+--build-arg WITH_CONTRIB=true
